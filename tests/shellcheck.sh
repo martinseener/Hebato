d9edcdd526aa745d769704b8118b80a14a8d6c21
Martin Seener <martin.seener@barzahlen.de>
2016-07-06 13:56:35 +0200

    Fixed Shellcheck test

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Executing Shellcheck (http://www.shellcheck.net/)"
-if shellcheck ../hebato.sh; then
+if shellcheck hebato.sh; then
     echo "Shellcheck exited without errors."
 fi
